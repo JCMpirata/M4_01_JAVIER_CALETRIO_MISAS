30b5019007c339aade60e21a9abaa5d3e5842bfd
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-12-09 18:03:03 +0100

    registros

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,6 +1,18 @@
 import pandas as pd
 
+#1. Mostrar los 5 primeros registros de cada tabla
 titulos = pd.read_csv('imdb_titulos.csv')
 titulos.head(5)
 elenco = pd.read_csv('imdb_elenco.csv')
 elenco.head(5)
+
+# 2. ¿Cuántos registros hay en cada tabla?
+numero_registros_titulos = len(titulos.axes[0])
+print("Numero de registros en titulos: ", numero_registros_titulos)
+numero_registros_elenco = len(elenco.axes[0])
+print("Numero de registros en elenco: ", numero_registros_elenco)
+
+# 3. Mostrar las 5 películas mas antiguas
+titulos.nsmallest(5, 'year')
+
+
